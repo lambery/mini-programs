a017c6ef8e9cf573cea29c02106fcdf02a1c120c
lambery <591169978@qq.com>
2018-10-24 22:38:57 +0800

    1
    1

--- demo/pages/posts/post.js
+++ demo/pages/posts/post.js
@@ -1,9 +1,74 @@
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
     indicatorDots: true,
     autoplay: true,
     interval: 2000,
     duration: 1000,
-    indicatorActiveColor:'#ddd'
+    indicatorActiveColor: '#ddd',
+    postTitle: "霞飞卸妆啦啦啦"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var post_content=[
+      { date: "Sep 18 2017" },
+      {date: "Sep 19 2017"}
+    ]
+    this.setData({postKey:post_content})
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
   }
 })
