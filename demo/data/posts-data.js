e7d86fc3db5618e1d0c41ba89fd4e9338957d7e5
lambery <591169978@qq.com>
2018-10-25 22:10:36 +0800

    2
    2

---
+++ demo/data/posts-data.js
@@ -0,0 +1,13 @@
+var post_content = [{
+  date: "Sep 17 2017"
+},
+{
+  date: "Sep 19 2017"
+}
+]
+
+var a1= "2"
+module.exports = {
+  postList: post_content,
+  keya: a1
+}
--- demo/pages/posts/post.js
+++ demo/pages/posts/post.js
@@ -1,3 +1,5 @@
+var postSData = require("../../data/posts-data.js")
+
 Page({
 
   /**
@@ -15,60 +17,60 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    var post_content=[
-      { date: "Sep 18 2017" },
-      {date: "Sep 19 2017"}
-    ]
-    this.setData({postKey:post_content})
+  onLoad: function(options) {
+
+    //this.data.postList = postSData.postList
+    this.setData({
+      postKey: postSData.postList
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-    
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-    
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-    
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-    
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-    
+  onShareAppMessage: function() {
+
   }
 })
