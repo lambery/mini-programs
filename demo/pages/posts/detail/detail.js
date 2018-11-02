3c84468a2d09a70aeeccbe5fadb4fefb273b0949
lambery <591169978@qq.com>
2018-11-02 22:49:04 +0800

    1
    1

--- demo/pages/posts/detail/detail.js
+++ demo/pages/posts/detail/detail.js
@@ -39,27 +39,45 @@ Page({
   onColl: function(event) {
     var postsCollected = wx.getStorageSync('posts_collected');
     var shouchang = postsCollected[this.data.postId];
-    shouchang = !shouchang;
-    postsCollected[this.data.postId] = shouchang;
-    wx.setStorageSync('posts_collected', postsCollected);
-    this.setData({
-      conllected: shouchang
-    });
-    if (shouchang == true){
-      wx.showToast({
-        title: '收藏成功',
-        icon: 'success',
-        duration: 1000
-      })
-    } else {
-      wx.showToast({
-        title: '收藏取消',
-        icon: 'none',
-        duration: 1000
-      })
-    }
-   
+    this.showModal(postsCollected, shouchang);
   },
+  showModal: function (postsCollected, shouchang) {
+    var that = this;
+    wx.showModal({
+      title: '提示',
+      content: '收藏与否',
+      cancelText: '不收藏',
+      confirmText: '收藏',
+      success(res) {
+        if (res.confirm) {
+          shouchang = true;
+          postsCollected[that.data.postId] = shouchang;
+          wx.setStorageSync('posts_collected', postsCollected);
+          that.setData({
+            conllected: shouchang
+          });
+          wx.showToast({
+            title: '收藏成功',
+            icon: 'success',
+            duration: 1000
+          })
+        } else if (res.cancel) {
+          shouchang = false;
+          postsCollected[that.data.postId] = shouchang;
+          wx.setStorageSync('posts_collected', postsCollected);
+          that.setData({
+            conllected: shouchang
+          });
+          wx.showToast({
+            title: '收藏取消',
+            icon: 'none',
+            duration: 1000
+          })
+        }
+      }
+    })
+  },
+
   // onColl2: function(event){
   //   wx.removeStorageSync('key');
   //   //wx.clearStorageSync();清除所有
